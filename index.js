0f90765b6e5e3076544e21b593a228586176e1de
Adam Mwaniki <adamndegwa@protonmail.com>
2024-03-21 16:19:59 +0300

    Completes the code along task

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+//#1
+function saturdayFun (activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`)
+}
+
+//#2
+function mondayWork (activity = "go to the office") {
+    return (`This Monday, I will ${activity}.`)
+}
+
+//#3
+//make a function that returns an anonymous inner function since everything is handled within the function scope as far as i can see
+//make a parameter called wrapper with a placeholder called *
+//the inner function has the parameter called adjective
+//the inner function returns the string as required using interpolation 
+function wrapAdjective(wrapper = "*") {
+     return function(adjective) {
+        return `You are ${wrapper}${adjective}${wrapper}!`;
+    };
+}
